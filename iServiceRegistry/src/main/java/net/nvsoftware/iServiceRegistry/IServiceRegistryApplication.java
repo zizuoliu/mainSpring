1e03ca3bb1e89cdcd3a9a56d193b05514da205b8
caleb <calebliu2014@gmail.com>
2023-05-02 15:14:43 -0700

    Init ServiceRegistry

---
+++ iServiceRegistry/src/main/java/net/nvsoftware/iServiceRegistry/IServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.iServiceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ iServiceRegistry/src/test/java/net/nvsoftware/iServiceRegistry/IServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.iServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
