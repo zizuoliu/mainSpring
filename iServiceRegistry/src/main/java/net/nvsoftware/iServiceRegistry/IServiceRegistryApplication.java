a0d4ac5f4881354f6ccb2fbffb6dbd755074ad9a
caleb <calebliu2014@gmail.com>
2023-05-02 15:20:13 -0700

    @EnableEurekaServer anotation in Application class

--- iServiceRegistry/src/main/java/net/nvsoftware/iServiceRegistry/IServiceRegistryApplication.java
+++ iServiceRegistry/src/main/java/net/nvsoftware/iServiceRegistry/IServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package net.nvsoftware.iServiceRegistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class IServiceRegistryApplication {
 
 	public static void main(String[] args) {
