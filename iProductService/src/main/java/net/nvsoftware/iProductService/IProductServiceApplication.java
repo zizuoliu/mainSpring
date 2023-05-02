85de67b42e60d9dab5d4ee172edb978eef0382a9
caleb <calebliu2014@gmail.com>
2023-05-02 14:12:33 -0700

    Init iProductService

---
+++ iProductService/src/main/java/net/nvsoftware/iProductService/IProductServiceApplication.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.iProductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IProductServiceApplication.class, args);
+	}
+
+}
---
+++ iProductService/src/test/java/net/nvsoftware/iProductService/IProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.iProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
