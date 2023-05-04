a19869ffd5945dd2fa6130f616a938fa2c526a50
Zizuo <liuzizuo@gmail.com>
2023-05-04 16:22:07 -0700

    Init APIGateway and config yaml routes

---
+++ iApiGateway/src/main/java/net/nvsoftware/iApiGateway/IApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.iApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IApiGatewayApplication.class, args);
+	}
+
+}
---
+++ iApiGateway/src/test/java/net/nvsoftware/iApiGateway/IApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.iApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
